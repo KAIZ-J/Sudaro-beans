85df8ec02bad33b9fb46e40e5c634a888ead3bf7
atom-x <faizjemal49@gmail.com>
2025-10-03 18:54:53 +0300

    commit

--- script.js
+++ script.js
@@ -11,6 +11,7 @@ const observer = new IntersectionObserver((entries) => {
 const cards = document.querySelectorAll(".card");
 cards.forEach((el) => observer.observe(el));
 document.getElementById("follow-us-section").querySelectorAll("a").forEach((el) => observer.observe(el));
+observer.observe(document.getElementById("comment-form"))
 function callPhone() {
   window.location.href = `tel:+251956334243`;
 }
